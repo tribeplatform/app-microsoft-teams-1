58839379f075b34390ff519a6046b1189632e124
Hamid Reza Khoramrokh <hr.kh1380@gmail.com>
2023-08-22 14:36:12 -0400

    made the sub funcs but havent done test

--- src/logics/subscriptions/index.ts
+++ src/logics/subscriptions/index.ts
@@ -5,8 +5,16 @@ import { Network } from '@tribeplatform/gql-client/types'
 
 import { globalLogger } from '@utils'
 import { handleNetworkSubscription } from './network'
-import { handlePostCreated } from './network/handleEventsTeams'
+import {
+  handlePostCreated,
+  memberInvitation,
+  memberShipSpace,
+  memberVerified,
+  modration,
+  spaceJoinRequest,
+} from './network/handleEventsTeams'
 import { ChannelRepository } from '@/repositories/channel.repository'
+import { getSpaces } from '../interactions/dynamic-blocks/settings/microsoft-info.logic'
 
 const logger = globalLogger.setContext(`Subscription`)
 
@@ -14,62 +22,187 @@ export const handleSubscriptionWebhook = async (
   webhook: SubscriptionWebhook,
 ): Promise<GeneralWebhookResponse> => {
   logger.debug('handleSubscriptionWebhook called', { webhook })
-
+  let channels: string[] = []
+  let member
+  let spaceId
+  let spacesList
+  let space
+  let mode
+  let actor
+  let title
+  let url
+  let self
   const {
     data: { name },
-    
   } = webhook
   const channleReps = await ChannelRepository.findMany()
   console.log(channleReps)
   try {
     switch (name) {
-      // case EventNoun.NETWORK:
-      //   await handleNetworkSubscription(webhook as SubscriptionWebhook<Network>)
       case 'post.published':
-        if (!webhook.data.object.isReply){
-        console.log('post.published', webhook.data.object, webhook.entities.actor)
-        const actor = webhook.entities.actor.name
-        const title = webhook.data.object.title
-        const url = webhook.data.object.url
-        let chanels: string[] = []
-        channleReps.forEach((chanel) => { console.log(chanel)
-           if (chanel.post == true) chanels.push(chanel.channelId) })
-        handlePostCreated(actor, url, title, chanels)
+        if (!webhook.data.object.isReply) {
+          console.log('post.published', webhook.data.object, webhook.entities.actor)
+          actor = webhook.entities.actor.name
+          title = webhook.data.object.title
+          url = webhook.data.object.url
+
+          channleReps.forEach(channel => {
+            console.log(channel)
+            if (channel.post == true && webhook.data.object.spaceId == channel.spaceIds)
+              channels.push(channel.channelId)
+          })
+          handlePostCreated(actor, url, title, channels)
         }
-        // return {
-        //   type: webhook.type,
-        //   status: WebhookStatus.Succeeded,
-    
-        // }
         break
       case 'moderation.created':
+        member = webhook.entities.targetMember.name
+        mode = 'created'
+        channleReps.forEach(channel => {
+          console.log(channel)
+          if (
+            channel.modarationCreated == true &&
+            webhook.data.object.spaceId == channel.spaceIds
+          )
+            channels.push(channel.channelId)
+        })
+        await modration({ member: member }, mode, url, channels)
         console.log('moderation.created')
         break
       case 'moderation.rejected':
+        actor = webhook.entities.actor.name
+        mode = 'rejected'
+        channleReps.forEach(channel => {
+          console.log(channel)
+          if (
+            channel.modarationRejected == true &&
+            webhook.data.object.spaceId == channel.spaceIds
+          )
+            channels.push(channel.channelId)
+        })
+        await modration({ actor: actor }, mode, url, channels)
         console.log('moderation.rejected')
         break
       case 'moderation.accepted':
+        actor = webhook.entities.actor.name
+        mode = 'accepted'
+        channleReps.forEach(channel => {
+          console.log(channel)
+          if (
+            channel.modarationAccepted == true &&
+            webhook.data.object.spaceId == channel.spaceIds
+          )
+            channels.push(channel.channelId)
+        })
+        await modration({ actor: actor }, mode, url, channels)
         console.log('moderation.accepted')
         break
       case 'space_membership.created':
+        actor = webhook.entities.actor.name
+        member = webhook.entities.targetMember.name
+        spaceId = webhook.data.object.spaceId
+        spacesList = await getSpaces(webhook.networkId)
+        space = spacesList.find(space => space.id == spaceId).name
+        self = false
+        mode = 'created'
+        if (webhook.entities.actor.id == webhook.entities.targetMember.id) {
+          self = true
+        }
+        channleReps.forEach(channel => {
+          console.log(channel)
+          if (
+            channel.spaceMemberCreated == true &&
+            webhook.data.object.spaceId == channel.spaceIds
+          )
+            channels.push(channel.channelId)
+        })
+        await memberShipSpace({ member: member, space: space, actor: actor },self, mode, channels)
         console.log('space_membership.created')
         break
       case 'space_membership.deleted':
+        actor = webhook.entities.actor.name
+        member = webhook.entities.targetMember.name
+        spaceId = webhook.data.object.spaceId
+        spacesList = await getSpaces(webhook.networkId)
+        space = spacesList.find(space => space.id == spaceId).name
+        self = false
+        mode = 'deleted'
+        if (webhook.entities.actor.id == webhook.entities.targetMember.id) {
+          self = true
+        }
+        channleReps.forEach(channel => {
+          console.log(channel)
+          if (
+            channel.spaceMemberDeleted == true &&
+            webhook.data.object.spaceId == channel.spaceIds
+          )
+            channels.push(channel.channelId)
+        })
+        await memberShipSpace({ member: member, space: space, actor: actor },self, mode, channels)
         console.log('space_membership.deleted')
         break
       case 'space_join_request.created':
+        member = webhook.entities.targetMember.name
+        spaceId = webhook.data.object.spaceId
+        spacesList = await getSpaces(webhook.networkId)
+        space = spacesList.find(space => space.id == spaceId).name
+        mode = 'created'
+        channleReps.forEach(channel => {
+          console.log(channel)
+          if (
+            channel.spaceJoinRequestCreated == true &&
+            webhook.data.object.spaceId == channel.spaceIds
+          )
+            channels.push(channel.channelId)
+        })
+        await spaceJoinRequest({ member: member, space: space }, mode, channels)
         console.log('space_join_request.created')
         break
       case 'space_join_request.accepted':
+        actor = webhook.entities.actor.name
+        member = webhook.entities.targetMember.name
+        spaceId = webhook.data.object.spaceId
+        spacesList = await getSpaces(webhook.networkId)
+        space = spacesList.find(space => space.id == spaceId).name
+        mode = 'accepted'
+        channleReps.forEach(channel => {
+          if (
+            channel.spaceJoinRequestAccepted == true &&
+            webhook.data.object.spaceId == channel.spaceIds
+          )
+            channels.push(channel.channelId)
+        })
+        await spaceJoinRequest(
+          { member: member, space: space, actor: actor },
+          mode,
+          channels,
+        )
         console.log('space_join_request.accepted')
         break
       case 'member_invitation.created':
+        const nameSender = webhook.entities.actor.name
+        const nameReciver = webhook.data.object.inviteeEmail
+        channleReps.forEach(channel => {
+          if (
+            channel.memberInvitionAccepted == true &&
+            webhook.data.object.spaceId == channel.spaceIds
+          )
+            channels.push(channel.channelId)
+        })
+        await memberInvitation({ actor: nameSender, member: nameReciver }, channels)
         console.log('member_invitation.created')
         break
       case 'member.verified':
+        channleReps.forEach(channel => {
+          if (
+            channel.memberVerified == true &&
+            webhook.data.object.spaceId == channel.spaceIds
+          )
+            channels.push(channel.channelId)
+        })
+        const message = webhook.data.object.name
+        await memberVerified({ member: message }, channels)
         console.log('member.verified')
         break
-      
     }
   } catch (error) {
     logger.error(error)
--- src/logics/subscriptions/network/handleEventsTeams.ts
+++ src/logics/subscriptions/network/handleEventsTeams.ts
@@ -1,10 +1,63 @@
-import { sendProactiveMessage } from "@/logics/oauth.logic";
-import { SubscriptionWebhook } from "@interfaces";
-import { Network } from "@prisma/client";
+import { sendProactiveMessage } from '@/logics/oauth.logic'
 
+export const handlePostCreated = async (name, url, title, channels): Promise<void> => {
+  const message = `${name} added a post with the title ${title}!`
+  await sendProactiveMessage(message, channels, url)
+}
 
-export const handlePostCreated = async ( name, url, title, chanels): Promise<void> => {
-    const message = `${name} added a post with the title ${title}!`
-    await sendProactiveMessage(message, chanels, url)
-    
+export const memberShipSpace = async (
+  messag: any,
+  self,
+  mode,
+  channels,
+): Promise<void> => {
+  let message
+  if (mode == 'created') {
+    if (self == true) {
+      message = `${messag.member} joined ${messag.space}`
+    } else {
+      message = `${messag.actor} added ${messag.member} to ${messag.space} `
+    }
+    await sendProactiveMessage(message, channels)
+  } else if (mode == 'deleted') {
+    if (self == true) {
+      message = `${messag.member} left ${messag.space}`
+    } else {
+      message = `${messag.actor} removed ${messag.member} from ${messag.space}`
+    }
+    await sendProactiveMessage(message, channels)
+  }
+}
+
+export const modration = async (messag: any, mode, url, channels): Promise<void> => {
+  if (mode == 'created') {
+    const message = `${messag.member} was flagged for moderation`
+    await sendProactiveMessage(message, channels, url)
+  } else if (mode == 'rejected') {
+    const message = `${messag.actor} approved this post`
+    await sendProactiveMessage(message, channels, url)
+  } else if (mode == 'accepted') {
+    const message = `${messag.actor} rejected this post`
+    await sendProactiveMessage(message, channels, url)
+  }
+}
+
+export const spaceJoinRequest = async (messag: any, mode, channels): Promise<void> => {
+  if (mode == 'created') {
+    const message = `${messag.member} requested to join ${messag.space}`
+    await sendProactiveMessage(message, channels)
+  } else if (mode == 'accepted') {
+    const message = `${messag.actor} accepted ${messag.member}'s request to join ${messag.space}`
+    await sendProactiveMessage(message, channels)
+  }
+}
+
+export const memberInvitation = async (messag: any, channels): Promise<void> => {
+  const message = `${messag.actor} invited ${messag.member} to the community.`
+  await sendProactiveMessage(message, channels)
+}
+
+export const memberVerified = async (messag: any, channels): Promise<void> => {
+  const message = `${messag.member} joined the community.`
+  await sendProactiveMessage(message, channels)
 }
