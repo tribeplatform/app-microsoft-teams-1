30e4d1a2d8bc4a31f41c7f870998c770de5b1f75
Hamid Reza Khoramrokh <hr.kh1380@gmail.com>
2023-08-24 17:12:16 -0400

    deleted unncecery declrations

--- src/logics/subscriptions/index.ts
+++ src/logics/subscriptions/index.ts
@@ -29,20 +29,10 @@ export const handleSubscriptionWebhook = async (
   webhook: SubscriptionWebhook,
 ): Promise<GeneralWebhookResponse> => {
   logger.debug('handleSubscriptionWebhook called', { webhook })
-  let channels: string[] = []
-  let member
-  // let spaceId
-  let spacesList
-  // let space
-  let mode
-  let actor
-  let title
-  let url
-  let self
+
   const {
     data: { noun },
   } = webhook
-
 
   try {
     switch (noun) {
@@ -70,175 +60,174 @@ export const handleSubscriptionWebhook = async (
       default:
         break
     }
-  } 
-  // try {
-  //   switch (noun) {
-  //     case 'post.published':
-  //       if (!webhook.data.object.isReply) {
-  //         console.log('post.published', webhook.data.object, webhook.entities.actor)
-  //         actor = webhook.entities.actor.name
-  //         title = webhook.data.object.title
-  //         url = webhook.data.object.url
+  } catch (error) {
+    // try {
+    //   switch (noun) {
+    //     case 'post.published':
+    //       if (!webhook.data.object.isReply) {
+    //         console.log('post.published', webhook.data.object, webhook.entities.actor)
+    //         actor = webhook.entities.actor.name
+    //         title = webhook.data.object.title
+    //         url = webhook.data.object.url
 
-  //         channleReps.forEach(channel => {
-  //           // console.log(channel)
-  //           if (channel.post == true && webhook.data.object.spaceId == channel.spaceIds)
-  //             channels.push(channel.channelId)
-  //         })
-  //         handlePostCreated(actor, url, title, channels)
-  //       }
-  //       break
-  //     case 'moderation.created':
-  //       member = webhook.entities.targetMember.name
-  //       mode = 'created'
-  //       channleReps.forEach(channel => {
-  //         console.log(channel)
-  //         if (
-  //           channel.modarationCreated == true &&
-  //           webhook.data.object.spaceId == channel.spaceIds
-  //         )
-  //           channels.push(channel.channelId)
-  //       })
-  //       await modration({ member: member }, mode, url, channels)
-  //       console.log('moderation.created')
-  //       break
-  //     case 'moderation.rejected':
-  //       actor = webhook.entities.actor.name
-  //       mode = 'rejected'
-  //       channleReps.forEach(channel => {
-  //         console.log(channel)
-  //         if (
-  //           channel.modarationRejected == true &&
-  //           webhook.data.object.spaceId == channel.spaceIds
-  //         )
-  //           channels.push(channel.channelId)
-  //       })
-  //       await modration({ actor: actor }, mode, url, channels)
-  //       console.log('moderation.rejected')
-  //       break
-  //     case 'moderation.accepted':
-  //       actor = webhook.entities.actor.name
-  //       mode = 'accepted'
-  //       channleReps.forEach(channel => {
-  //         console.log(channel)
-  //         if (
-  //           channel.modarationAccepted == true &&
-  //           webhook.data.object.spaceId == channel.spaceIds
-  //         )
-  //           channels.push(channel.channelId)
-  //       })
-  //       await modration({ actor: actor }, mode, url, channels)
-  //       console.log('moderation.accepted')
-  //       break
-  //     case 'space_membership.created':
-  //       actor = webhook.entities.actor.name
-  //       member = webhook.entities.targetMember.name
-  //       spaceId = webhook.data.object.spaceId
-  //       spacesList = await getSpaces(webhook.networkId)
-  //       space = spacesList.find(space => space.id == spaceId).name
-  //       self = false
-  //       mode = 'created'
-  //       if (webhook.entities.actor.id == webhook.entities.targetMember.id) {
-  //         self = true
-  //       }
-  //       channleReps.forEach(channel => {
-  //         console.log(channel)
-  //         if (
-  //           channel.spaceMemberCreated == true &&
-  //           webhook.data.object.spaceId == channel.spaceIds
-  //         )
-  //           channels.push(channel.channelId)
-  //       })
-  //       await memberShipSpace({ member: member, space: space, actor: actor },self, mode, channels)
-  //       console.log('space_membership.created')
-  //       break
-  //     case 'space_membership.deleted':
-  //       actor = webhook.entities.actor.name
-  //       member = webhook.entities.targetMember.name
-  //       spaceId = webhook.data.object.spaceId
-  //       spacesList = await getSpaces(webhook.networkId)
-  //       space = spacesList.find(space => space.id == spaceId).name
-  //       self = false
-  //       mode = 'deleted'
-  //       if (webhook.entities.actor.id == webhook.entities.targetMember.id) {
-  //         self = true
-  //       }
-  //       channleReps.forEach(channel => {
-  //         console.log(channel)
-  //         if (
-  //           channel.spaceMemberDeleted == true &&
-  //           webhook.data.object.spaceId == channel.spaceIds
-  //         )
-  //           channels.push(channel.channelId)
-  //       })
-  //       await memberShipSpace({ member: member, space: space, actor: actor },self, mode, channels)
-  //       console.log('space_membership.deleted')
-  //       break
-  //     case 'space_join_request.created':
-  //       member = webhook.entities.targetMember.name
-  //       spaceId = webhook.data.object.spaceId
-  //       spacesList = await getSpaces(webhook.networkId)
-  //       space = spacesList.find(space => space.id == spaceId).name
-  //       mode = 'created'
-  //       channleReps.forEach(channel => {
-  //         console.log(channel)
-  //         if (
-  //           channel.spaceJoinRequestCreated == true &&
-  //           webhook.data.object.spaceId == channel.spaceIds
-  //         )
-  //           channels.push(channel.channelId)
-  //       })
-  //       await spaceJoinRequest({ member: member, space: space }, mode, channels)
-  //       console.log('space_join_request.created')
-  //       break
-  //     case 'space_join_request.accepted':
-  //       actor = webhook.entities.actor.name
-  //       member = webhook.entities.targetMember.name
-  //       spaceId = webhook.data.object.spaceId
-  //       spacesList = await getSpaces(webhook.networkId)
-  //       space = spacesList.find(space => space.id == spaceId).name
-  //       mode = 'accepted'
-  //       channleReps.forEach(channel => {
-  //         if (
-  //           channel.spaceJoinRequestAccepted == true &&
-  //           webhook.data.object.spaceId == channel.spaceIds
-  //         )
-  //           channels.push(channel.channelId)
-  //       })
-  //       await spaceJoinRequest(
-  //         { member: member, space: space, actor: actor },
-  //         mode,
-  //         channels,
-  //       )
-  //       console.log('space_join_request.accepted')
-  //       break
-  //     case 'member_invitation.created':
-  //       const nameSender = webhook.entities.actor.name
-  //       const nameReciver = webhook.data.object.inviteeEmail
-  //       channleReps.forEach(channel => {
-  //         if (
-  //           channel.memberInvitionAccepted == true &&
-  //           webhook.data.object.spaceId == channel.spaceIds
-  //         )
-  //           channels.push(channel.channelId)
-  //       })
-  //       await memberInvitation({ actor: nameSender, member: nameReciver }, channels)
-  //       console.log('member_invitation.created')
-  //       break
-  //     case 'member.verified':
-  //       channleReps.forEach(channel => {
-  //         if (
-  //           channel.memberVerified == true &&
-  //           webhook.data.object.spaceId == channel.spaceIds
-  //         )
-  //           channels.push(channel.channelId)
-  //       })
-  //       const message = webhook.data.object.name
-  //       await memberVerified({ member: message }, channels)
-  //       console.log('member.verified')
-  //       break
-  //   }
-   catch (error) {
+    //         channleReps.forEach(channel => {
+    //           // console.log(channel)
+    //           if (channel.post == true && webhook.data.object.spaceId == channel.spaceIds)
+    //             channels.push(channel.channelId)
+    //         })
+    //         handlePostCreated(actor, url, title, channels)
+    //       }
+    //       break
+    //     case 'moderation.created':
+    //       member = webhook.entities.targetMember.name
+    //       mode = 'created'
+    //       channleReps.forEach(channel => {
+    //         console.log(channel)
+    //         if (
+    //           channel.modarationCreated == true &&
+    //           webhook.data.object.spaceId == channel.spaceIds
+    //         )
+    //           channels.push(channel.channelId)
+    //       })
+    //       await modration({ member: member }, mode, url, channels)
+    //       console.log('moderation.created')
+    //       break
+    //     case 'moderation.rejected':
+    //       actor = webhook.entities.actor.name
+    //       mode = 'rejected'
+    //       channleReps.forEach(channel => {
+    //         console.log(channel)
+    //         if (
+    //           channel.modarationRejected == true &&
+    //           webhook.data.object.spaceId == channel.spaceIds
+    //         )
+    //           channels.push(channel.channelId)
+    //       })
+    //       await modration({ actor: actor }, mode, url, channels)
+    //       console.log('moderation.rejected')
+    //       break
+    //     case 'moderation.accepted':
+    //       actor = webhook.entities.actor.name
+    //       mode = 'accepted'
+    //       channleReps.forEach(channel => {
+    //         console.log(channel)
+    //         if (
+    //           channel.modarationAccepted == true &&
+    //           webhook.data.object.spaceId == channel.spaceIds
+    //         )
+    //           channels.push(channel.channelId)
+    //       })
+    //       await modration({ actor: actor }, mode, url, channels)
+    //       console.log('moderation.accepted')
+    //       break
+    //     case 'space_membership.created':
+    //       actor = webhook.entities.actor.name
+    //       member = webhook.entities.targetMember.name
+    //       spaceId = webhook.data.object.spaceId
+    //       spacesList = await getSpaces(webhook.networkId)
+    //       space = spacesList.find(space => space.id == spaceId).name
+    //       self = false
+    //       mode = 'created'
+    //       if (webhook.entities.actor.id == webhook.entities.targetMember.id) {
+    //         self = true
+    //       }
+    //       channleReps.forEach(channel => {
+    //         console.log(channel)
+    //         if (
+    //           channel.spaceMemberCreated == true &&
+    //           webhook.data.object.spaceId == channel.spaceIds
+    //         )
+    //           channels.push(channel.channelId)
+    //       })
+    //       await memberShipSpace({ member: member, space: space, actor: actor },self, mode, channels)
+    //       console.log('space_membership.created')
+    //       break
+    //     case 'space_membership.deleted':
+    //       actor = webhook.entities.actor.name
+    //       member = webhook.entities.targetMember.name
+    //       spaceId = webhook.data.object.spaceId
+    //       spacesList = await getSpaces(webhook.networkId)
+    //       space = spacesList.find(space => space.id == spaceId).name
+    //       self = false
+    //       mode = 'deleted'
+    //       if (webhook.entities.actor.id == webhook.entities.targetMember.id) {
+    //         self = true
+    //       }
+    //       channleReps.forEach(channel => {
+    //         console.log(channel)
+    //         if (
+    //           channel.spaceMemberDeleted == true &&
+    //           webhook.data.object.spaceId == channel.spaceIds
+    //         )
+    //           channels.push(channel.channelId)
+    //       })
+    //       await memberShipSpace({ member: member, space: space, actor: actor },self, mode, channels)
+    //       console.log('space_membership.deleted')
+    //       break
+    //     case 'space_join_request.created':
+    //       member = webhook.entities.targetMember.name
+    //       spaceId = webhook.data.object.spaceId
+    //       spacesList = await getSpaces(webhook.networkId)
+    //       space = spacesList.find(space => space.id == spaceId).name
+    //       mode = 'created'
+    //       channleReps.forEach(channel => {
+    //         console.log(channel)
+    //         if (
+    //           channel.spaceJoinRequestCreated == true &&
+    //           webhook.data.object.spaceId == channel.spaceIds
+    //         )
+    //           channels.push(channel.channelId)
+    //       })
+    //       await spaceJoinRequest({ member: member, space: space }, mode, channels)
+    //       console.log('space_join_request.created')
+    //       break
+    //     case 'space_join_request.accepted':
+    //       actor = webhook.entities.actor.name
+    //       member = webhook.entities.targetMember.name
+    //       spaceId = webhook.data.object.spaceId
+    //       spacesList = await getSpaces(webhook.networkId)
+    //       space = spacesList.find(space => space.id == spaceId).name
+    //       mode = 'accepted'
+    //       channleReps.forEach(channel => {
+    //         if (
+    //           channel.spaceJoinRequestAccepted == true &&
+    //           webhook.data.object.spaceId == channel.spaceIds
+    //         )
+    //           channels.push(channel.channelId)
+    //       })
+    //       await spaceJoinRequest(
+    //         { member: member, space: space, actor: actor },
+    //         mode,
+    //         channels,
+    //       )
+    //       console.log('space_join_request.accepted')
+    //       break
+    //     case 'member_invitation.created':
+    //       const nameSender = webhook.entities.actor.name
+    //       const nameReciver = webhook.data.object.inviteeEmail
+    //       channleReps.forEach(channel => {
+    //         if (
+    //           channel.memberInvitionAccepted == true &&
+    //           webhook.data.object.spaceId == channel.spaceIds
+    //         )
+    //           channels.push(channel.channelId)
+    //       })
+    //       await memberInvitation({ actor: nameSender, member: nameReciver }, channels)
+    //       console.log('member_invitation.created')
+    //       break
+    //     case 'member.verified':
+    //       channleReps.forEach(channel => {
+    //         if (
+    //           channel.memberVerified == true &&
+    //           webhook.data.object.spaceId == channel.spaceIds
+    //         )
+    //           channels.push(channel.channelId)
+    //       })
+    //       const message = webhook.data.object.name
+    //       await memberVerified({ member: message }, channels)
+    //       console.log('member.verified')
+    //       break
+    //   }
     logger.error(error)
     return {
       type: webhook.type,
